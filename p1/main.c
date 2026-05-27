41fe4c6611c464de5ae93826f429fc2b611b5c6b
2025129036 <azljycover3366@naver.com>
2026-05-27 15:14:10 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,26 @@
+#include <print>
+
+int main()
+{
+    char arr[11];
+    int max = 0;
+    char result = 0;
+    
+    scanf("%s", arr);
+    
+    for(char *p = arr; p < arr + 10; p++) {
+        int count = 0;
+        
+        for (char *q = arr; q < arr + 10; q++) {
+            if (*q == *p){
+                count++;
+            }
+        }
+            if (count > max){
+                max = count;
+                result = *p;
+        }
+    }
+    printf("%c %d", result, max);
+    return 0;
+}
